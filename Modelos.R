c9e731a31e4e32acea4f8521a4e836487661f163
Gonzalo Sabatino <gonzalosabatino@gsabatino.local>
2023-02-27 00:57:17 -0300

    c2

--- Modelos.R
+++ Modelos.R
@@ -3,6 +3,8 @@
 ## Importo librerías
 load_libraries <- function() {
   library(MASS)
+  library(jtools)
+  library(broom.mixed)
   library(caret)
   library(ISLR2)
   library(carData)
@@ -21,11 +23,8 @@ load_libraries <- function() {
 }
 
 load_libraries()
-mat <- read.table("mat_transf.csv",sep=",",header=TRUE)
-por <- read.table("por_transf.csv",sep=",",header=TRUE)
-
-mat <- mat[, -1]
-por <- por[, -1]
+mat <- read.table("student-mat.csv",sep=",",header=TRUE)
+por <- read.table("student-por.csv",sep=",",header=TRUE)
 
 mat[-c(3,7,8,13,14,15,24,25,26,27,28,29,30,31,32,33)] <- 
   lapply(mat[-c(3,7,8,13,14,15,24,25,26,27,28,29,30,31,32,33)], factor)
@@ -51,21 +50,25 @@ train.P <- por[aux_train, ]
 test.P <- por[aux_test, ]
 
 # Análisis de variables ----
-library(stargazer)
-stargazer(mat, type="html", out="1.html")
-stargazer(mat, type = "html", nobs = FALSE, mean.sd = TRUE, median = TRUE,
-          iqr = TRUE, out="2.html")
+#library(stargazer)
+#stargazer(mat, type="html", out="1.html")
+#stargazer(mat, type = "html", nobs = FALSE, mean.sd = TRUE, median = TRUE,
+#         iqr = TRUE, out="2.html")
 
-library(psych)
-describeBy(mat$G1, mat$school, mat = T, digits = 2)
-describeBy(mat$G2, mat$school, mat = T, digits = 2)
-describeBy(mat$G3, mat$school, mat = T, digits = 2)
+## Descripción inicial ----
+describe(mat$G3)
 
 hist(x = mat$G3, breaks = seq(min(mat$G3), max(mat$G3), 1), 
      xlim = c(min(mat$G3), max(mat$G3)), prob=T, xaxt = "n")
 axis(1, at = seq(min(mat$G3), max(mat$G3), 1), 
      labels = seq(min(mat$G3), max(mat$G3), 1))
 curve(dnorm(x, mean = mean(mat$G3), sd = sd(mat$G3)), col="red", add = T)
+
+## Descripción de escuelas ----
+library(psych)
+describeBy(mat$G1, mat$school, mat = T, digits = 2)
+describeBy(mat$G2, mat$school, mat = T, digits = 2)
+describeBy(mat$G3, mat$school, mat = T, digits = 2)
 
 hist(x = mat[mat$school == "MS", ]$G3, breaks = seq(min(mat[mat$school == "MS", ]$G3), max(mat[mat$school == "MS", ]$G3), 1), 
      xlim = c(min(mat[mat$school == "MS", ]$G3), max(mat[mat$school == "MS", ]$G3)), prob=T, xaxt = "n")
@@ -79,8 +82,8 @@ axis(1, at = seq(min(mat[mat$school == "GP", ]$G3), max(mat[mat$school == "GP", 
      labels = seq(min(mat[mat$school == "GP", ]$G3), max(mat[mat$school == "GP", ]$G3), 1))
 curve(dnorm(x, mean = mean(mat[mat$school == "GP", ]$G3), sd = sd(mat[mat$school == "GP", ]$G3)), col="red", add = T)
 
-qqnorm(mat[mat$school == "GP", ]$G3); qqline(mat[mat$school == "GP", ]$G3, col = "red")
-qqnorm(mat[mat$school == "MS", ]$G3); qqline(mat[mat$school == "MS", ]$G3, col = "red")
+qqnorm(mat[mat$school == "GP", ]$G3, pch=20); qqline(mat[mat$school == "GP", ]$G3, col = "red")
+qqnorm(mat[mat$school == "MS", ]$G3, pch=20); qqline(mat[mat$school == "MS", ]$G3, col = "red")
 
 ggplot(data = mat, aes(x=school, y = G1)) +
   geom_boxplot(aes(fill=school)) +
@@ -103,26 +106,7 @@ ggplot(data = mat, aes(x=school, y = G3)) +
   scale_fill_manual(values=c("#0097BF", "#204FFF")) +
   scale_y_continuous(breaks=c(1:20))
 
-## Escalo absences
-hist(x = mat$absences, breaks = seq(min(mat$absences), max(mat$absences), 1), 
-     xlim = c(min(mat$absences), max(mat$absences)), prob=T, xaxt = "n")
-axis(1, at = seq(min(mat$absences), max(mat$absences), 1), 
-     labels = seq(min(mat$absences), max(mat$absences), 1))
-curve(dnorm(x, mean = mean(mat$absences), sd = sd(mat$absences)), col="red", add = T)
-describe(mat$absences)
-
-## Tomo raíz cuadrada
-mat$absences <- sqrt(mat$absences)
-
-hist(x = mat$absences, 
-     xlim = c(min(mat$absences), max(mat$absences)), prob=T, xaxt = "n")
-axis(1, at = seq(min(mat$absences), max(mat$absences), 1), 
-     labels = seq(min(mat$absences), max(mat$absences), 1))
-curve(dnorm(x, mean = mean(mat$absences), sd = sd(mat$absences)), col="red", add = T)
-
-describe(mat$absences)
-
-# Otro análisis ----
+## Correlación ----
 library(Hmisc)
 # Mat
 RawDataMatrix <- subset(train.M, select=c(age, Medu, Fedu, traveltime, studytime, failures,famrel, freetime, goout, Dalc, Walc, health, absences, G1, G2, G3))
@@ -138,49 +122,3 @@ corrplot(corMatrix2$r, p.mat = corMatrix2$P, sig.level = 0.05, insig = "blank",
          diag=F, type="lower")
 corrplot(corMatrix2$r, p.mat = corMatrix2$P, sig.level = 0.05, insig = "blank",
          order="hclust", addrect=2)
-
-
-# Outliers por variable ----
-# Mat
-attach(mat)
-boxplot(G3 ~ failures)
-box_plot <- function(data, x, y) {
-  p <- ggplot(data = data, aes(x=x, y = y)) +
-    geom_boxplot(aes(fill=x)) +
-    labs(title ="", y = "")+
-    theme(plot.title = element_text(size = 15, hjust=0.5))+
-    scale_fill_manual(values=c("#0097BF", "#204FFF")) +
-    scale_y_continuous(breaks=c(1:20))
-  
-  return(p)
-}
-
-box_plot(mat, school, G3)
-box_plot(mat, Medu, G3)
-
-# Otro 2 ----
-features <- setdiff(names(mat), "G3")
-pre_process <- preProcess(
-  x      = train.M[, features],
-  method = c("center", "scale")    
-)
-
-# Mat
-train_x <- predict(pre_process, train.M[, features])
-test_x <- predict(pre_process, test.M[, features])
-
-train_x$G3 <- train.M$G3
-test_x$G3 <- test.M$G3
-
-train.M <- train_x
-test.M <- test_x
-
-# Por
-train_x <- predict(pre_process, train.P[, features])
-test_x <- predict(pre_process, test.P[, features])
-
-train_x$G3 <- train.P$G3
-test_x$G3 <- test.P$G3
-
-train.P <- train_x
-test.P <- test_x
